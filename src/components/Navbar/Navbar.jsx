6cb4174a36b7a804e7d750394eddb2c7a376c4b8
Pratham <prathamarora9365@gmail.com>
2025-09-03 23:10:39 +0530

    uifix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-    
+
         <div className="navbar-logo">
           <h2>MockTankAI</h2>
         </div>
 
-        
-        <ul className="navbar-menu">
+        {/* Hamburger Toggle */}
+        <div className="navbar-toggle" onClick={() => setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Nav Menu */}
+        <ul className={`navbar-menu ${isOpen ? 'active' : ''}`}>
           <li className="navbar-item">
-            <Link to="/" className="navbar-link">Home</Link>
+            <Link to="/" className="navbar-link" onClick={() => setIsOpen(false)}>Home</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/simulation" className="navbar-link">Simulations</Link>
-          </li>
-          {/* <li className="navbar-item">
-            <Link to="#" className="navbar-link">Analysis</Link>
-          </li> */}
-          <li className="navbar-item">
-            <Link to="#" className="navbar-link">Recents</Link>
+            <Link to="/simulation" className="navbar-link" onClick={() => setIsOpen(false)}>Simulations</Link>
           </li>
           <li className="navbar-item">
-            <Link to="/feedback" className="navbar-link">Feedback</Link>
+            <Link to="#" className="navbar-link" onClick={() => setIsOpen(false)}>Recents</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/feedback" className="navbar-link" onClick={() => setIsOpen(false)}>Feedback</Link>
           </li>
         </ul>
 
-<Link to="/signin" className="navbar-signin-link">
-<div className="navbar-cta">
-          <button className="navbar-button">Get Started</button>
+        {/* CTA button stays right on desktop, collapses below menu on mobile */}
+        <div className={`navbar-cta ${isOpen ? 'active' : ''}`}>
+          <Link to="/signin" onClick={() => setIsOpen(false)}>
+            <button className="navbar-button">Get Started</button>
+          </Link>
         </div>
-</Link>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
